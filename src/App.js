3bdc30c2393665a1be445614df50e14ef94a138f
Nawin Vignesh <132936333+Nawinvignes@users.noreply.github.com>
2023-12-26 23:42:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes,Route } from 'react-router-dom';
+import { Signin } from './Components/Signin';
+import { Signup } from './Components/Signup';
+import ResponsiveAppBar, { Home } from './Components/Home';
+// import { Home } from './Components/Home';
+import './Assets/Home.css'
+import { HomeAfterSignin } from './Components/HomeAfterSignin.js';
+function App() {
+  return (
+    <div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<HomeAfterSignin/>}/>
+        <Route path='/Signin' element={<Signin/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
